2b878fc682321deaacbd117be86da1c0548d64e2
Jingxiang Mo <jingxiangmo@gmail.com>
2022-01-22 17:52:15 -0500

    Added resources

--- resources.py
+++ resources.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 
 st.title("RESOURCES TEST PAGE")
 
@@ -13,14 +14,39 @@ class Resource:
         self.description = description
         self.url = url
 
-    # Generate a function to display the resource
-
 p1 = Resource("Python", "Python is a programming language", "https://www.python.org/")
 p2 = Resource("Streamlit", "Streamlit is a web application framework", "https://streamlit.io/")
 p3 = Resource("Pandas", "Pandas is a data analysis library", "https://pandas.pydata.org/")
 
 
-def display():
+
+
+# choose a random article between 0 to 2
+random = random.randint(0, 2)
+
+
+# anger, fear, joy, love, sadness, surprise
+
+anger_resources = (
+    ("Happify", "Happify is a web application that helps you manage your mental health", "https://happify.io/"),
+    ("How to Meditate", "How to Meditate is a website that teaches you how to meditate", "https://www.mindful.org/how-to-meditate/"),
+    ("Running", "Running can lift your mood and help you feel more equipped to confront your anger", "https://www.self.com/story/rage-run"),
+    ("Meditation", "Meditation can help you feel more calm and relaxed", "https://www.mindful.org/meditation/"),
+    ("Anger Management Methods", "Anger Management Methods is a website that teaches you how to manage your anger", "https://www.self.com/article/anger-management-methods"),
+    )
+
+fear_resources = (
+    ("How to overcome fear and anxiety", "How to overcome fear and anxiety is a website that teaches you how to overcome fear and anxiety", "https://www.nhsinform.scot/healthy-living/mental-wellbeing/fears-and-phobias/ten-ways-to-fight-your-fears"),
+    ("Breathign techniques", "Try these breathing techniques to calm your mind", "https://www.mentalhealth.org.uk/publications/overcome-fear-anxiety"),
+    ("Overcome fear", "Here are 5 ways to overcome fear and setbacks", "https://www.tonyrobbins.com/stories/unleash-the-power/overcoming-fear-in-5-steps/"),
+    ("14 ways to overcome fear", "14 ways to overcome fear", "https://www.forbes.com/sites/joshsteimle/2016/01/04/14-ways-to-conquer-fear/"),
+    )
+
+
+
+
+
+def display_resources():
     with col1:
         st.header(p1.title)
         st.write(p1.description)
@@ -36,4 +62,4 @@ def display():
         st.write(p3.description)
         st.markdown("<a href=\"p3.url\"> Learn More </a>", unsafe_allow_html=True)
 
-display()
+display_resources()
