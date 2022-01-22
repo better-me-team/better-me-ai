4916dff9dc55f514043b65091874da3cb9eef283
rt-kill <roland.t.killian@gmail.com>
2022-01-22 17:11:42 -0500

    Add "Routes"

--- app.py
+++ app.py
@@ -3,24 +3,71 @@ import requests
 import json
 import time
 
-API_URL = "https://api-inference.huggingface.co/models/mrm8488/t5-base-finetuned-emotion"
-headers = {"Authorization": f"Bearer {rAplzyQGYLwcFPzUfSqVpGvRdvvXHrmfOitDsopymDDjoxtaOIEfDMeFALNMdDaNuQNIoPZfutTtqBCMlcRsDACtBUoHTsiPFsrQagnPmqyzKbJLAMBBTJTgLNpcvpOZ}"}
+# API_URL = "https://api-inference.huggingface.co/models/mrm8488/t5-base-finetuned-emotion"
+# headers = {"Authorization": f"Bearer {rAplzyQGYLwcFPzUfSqVpGvRdvvXHrmfOitDsopymDDjoxtaOIEfDMeFALNMdDaNuQNIoPZfutTtqBCMlcRsDACtBUoHTsiPFsrQagnPmqyzKbJLAMBBTJTgLNpcvpOZ}"}
 
-notes = []
+def main():
+    pages = {
+        "Home": page_home,
+        "Journal": page_journal,
+        "Analytics": page_analytics,
+    }
+    
+    if "page" not in st.session_state:
+        st.session_state.update({
+            # Default page
+            "page": "Home",
+            
+            # Notes already made for demo
+            "notes": [
+                "This is the first note you have ever made",
+                "This is your second note, congrats!",
+                "Note 3",
+                "You see where this is going"
+            ]
+            
+            # Default widget values
+            # "text": "",
+            # "slider": 0,
+            # "checkbox": False,
+            # "radio": "Hello",
+            # "selectbox": "Hello",
+            # "multiselect": ["Hello", "Everyone"],
+        })
+        
+    page = "Home"
 
-def mood_inference(note):
-    data = {"inputs": note}
-    response = requests.post(API_URL, json=data, headers=headers)
-    mood = response.json()
+    with st.sidebar:
+        st.title("Better.me ©")
+        if st.button("Home"): page = "Home"
+        if st.button("Journal"): page = "Journal"
+        if st.button("Analytics"): page = "Analytics"
+        
+    pages[page]()
 
-    notes.append(note)
-    pass
+
+def page_home():
+    st.title("Home")
 
 
-st.sidebar.title("Better.me ©")
+def page_journal():
+    def mood_inference(note):
+        data = {"inputs": note}
+        response = requests.post(API_URL, json=data, headers=headers)
+        mood = response.json()
+    
+        notes.append(note)
+        pass
+    
+    st.write("What's on your mind today?")
+    note = st.text_area("", value="...", max_chars=512, )
+    if st.button("Click here to add note"):
+        mood_inference(note)
+        
 
-st.write("What's on your mind today?")
-note = st.text_area("", value="...", max_chars=512, )
-if st.button("Click here to add note"):
-    mood_inference(note)
+def page_analytics():
+    st.title("Analytics")
 
+
+if __name__ == "__main__":
+    main()
