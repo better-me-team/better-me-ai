a951ab8bbea39028eb6fb80c94e04b0370813115
Jingxiang Mo <jingxiangmo@gmail.com>
2022-01-22 22:39:54 -0500

    Finished populating the site

--- links.py
+++ links.py
@@ -1,41 +1,68 @@
 anger_resources = (
-    ("Happify", "Happify is a web application that helps you manage your mental health", "https://happify.io/"),
+    ("Try Happify", "Happify is a web application that helps you manage your mental health", "https://happify.io/"),
     ("How to Meditate", "How to Meditate is a website that teaches you how to meditate",
      "https://www.mindful.org/how-to-meditate/"),
-    ("Running", "Running can lift your mood and help you feel more equipped to confront your anger",
+    ("Try Running", "Running can lift your mood and help you feel more equipped to confront your anger",
      "https://www.self.com/story/rage-run"),
-    ("Meditation", "Meditation can help you feel more calm and relaxed", "https://www.mindful.org/meditation/"),
+    ("Try Meditation", "Meditation can help you feel more calm and relaxed", "https://www.mindful.org/meditation/"),
     ("Anger Management Methods", "Anger Management Methods is a website that teaches you how to manage your anger",
      "https://www.self.com/article/anger-management-methods"),
+    ("Emotional Management Class", "Emotional Management Class is a website that teaches you how to manage your anger", "https://tomcaplanmsw.com/anger-management/")
 )
 
 fear_resources = (
     ("How to overcome fear and anxiety",
      "How to overcome fear and anxiety is a website that teaches you how to overcome fear and anxiety",
      "https://www.nhsinform.scot/healthy-living/mental-wellbeing/fears-and-phobias/ten-ways-to-fight-your-fears"),
-    ("Breathign techniques", "Try these breathing techniques to calm your mind",
+    ("Breathing techniques", "Try these breathing techniques to calm your mind",
      "https://www.mentalhealth.org.uk/publications/overcome-fear-anxiety"),
-    ("Overcome fear", "Here are 5 ways to overcome fear and setbacks",
+    ("Don't be afraid of fear", "Here are 5 ways to overcome fear and setbacks",
      "https://www.tonyrobbins.com/stories/unleash-the-power/overcoming-fear-in-5-steps/"),
-    ("14 ways to overcome fear", "14 ways to overcome fear",
-     "https://www.forbes.com/sites/joshsteimle/2016/01/04/14-ways-to-conquer-fear/"),
+    ("Preparing for Panics", "You may have a type of anxiety disorder called panic disorder. Left untreated, "
+                             "panic disorder can lower your quality of life",
+     "https://www.nimh.nih.gov/health/publications/panic-disorder-when-fear-overwhelms"),
+    ("How to deal with fear", "How to deal with fear is a website that teaches you how to deal with fear", "https"
+                                                                                                           "://www.mentalhealth.org.uk/publications/how-to-deal-with-fear"),
+    ("Successful Mental Health", "How intense is work right now? These simple and useful techniques may help lessen "
+                                 "your anxiety.",
+     "https://www.inc.com/marcel-schwantes/8-mental-hacks-that-will-keep-you-strong-under-control-during-tough-times"
+     ".html")
 )
 
 joy_resources = (
     ("How to be happy", "How to be happy is a website that teaches you how to be happy",
      "https://www.self.com/article/how-to-be-happy"),
-
+    ("Breathing techniques", "Try these breathing techniques to calm your mind",
+     "https://www.mentalhealth.org.uk/publications/overcome-fear-anxiety"),
+    ("Try Happify", "Happify is a web application that helps you manage your mental health", "https://happify.io/"),
+    ("Healthy Living Index", "Healthy Living Index is a website that teaches you how to be happy", "https://www.self.com/article/healthy-living-index"),
+    ("Happiness, Explained", "How to be happy is a website that teaches you how to be happy", "https://www.self.com/article/how-to-be-happy"),
+    ("You're Happy, now what?", "You're Happy, now what? is a website that teaches you how to be happy", "https://www.self.com/article/youre-happy-now-what")
 )
 
 love_resources = (
-
-
+    ("How to be in love", "How to be in love is a website that teaches you how to be in love", "https://www.self.com/article/how-to-be-in-love"),
+    ("Breathing techniques", "Try these breathing techniques to calm your mind", "https://www.mentalhealth.org.uk/publications/overcome-fear-anxiety"),
+    ("Try Happify", "Happify is a web application that helps you manage your mental health", "https://happify.io/"),
+    ("How to Calm your Nerves", "How to Calm your Nerves is a website that teaches you how to be in love", "https://www.self.com/article/how-to-calm-your-nerves"),
+    ("Benefits of Being In Love", "Love and positive social support increase feelings of happiness and reduce stress", "https://www.self.com/article/benefits-of-being-in-love"),
+    ("Love is in the Air", "Love is in the Air is a courses that teaches you how to be in love", "https://www.self.com/article/love-is-in-the-air")
 )
 
 sadness_resources = (
-
+    ("How to deal with sadness", "How to deal with sadness is a website that teaches you how to deal with sadness", "https://www.self.com/article/how-to-deal-with-sadness"),
+    ("Breathing techniques", "Try these breathing techniques to calm your mind", "https://www.mentalhealth.org.uk/publications/overcome-fear-anxiety"),
+    ("Try Happify", "Happify is a web application that helps you manage your mental health", "https://happify.io/"),
+    ("How to be happy", "How to be happy is a website that teaches you how to be happy", "https://www.self.com/article/how-to-be-happy"),
+    ("Coping with Sadness", "Coping with Sadness is a website that teaches you how to be happy", "https://www.self.com/article/coping-with-sadness"),
+    ("How to be happy", "How to be happy is a website that teaches you how to be happy", "https://www.self.com/article/how-to-be-happy")
 )
 
 surprise_resources = (
-
+    ("Breathing techniques", "Try these breathing techniques to calm your mind", "https://www.mentalhealth.org.uk/publications/overcome-fear-anxiety"),
+    ("Try Happify", "Happify is a web application that helps you manage your mental health", "https://happify.io/"),
+    ("Try Anxiety Relief", "Try Anxiety Relief is a website that teaches you how to deal with anxiety", "https://www.self.com/article/try-anxiety-relief"),
+    ("How to deal with anxiety", "How to deal with anxiety is a website that teaches you how to deal with anxiety", "https://www.self.com/article/how-to-deal-with-anxiety"),
+    ("Try Running", "Try Running is a website that teaches you how to deal with anxiety", "https://www.self.com/article/try-running"),
+    ("Meditation", "Meditation is a website that teaches you how to deal with anxiety", "https://www.self.com/article/meditation")
 )
