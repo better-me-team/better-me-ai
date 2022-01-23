b530f5f623c9428309fd3830342c9b9093d39538
rt-kill <69824969+rt-kill@users.noreply.github.com>
2022-01-22 21:41:52 -0500

    Revert "Added "Support" section"

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from random import randint
 import altair as alt
 from collections import Counter
 
-from resources import choose_resources, choose_support
+from resources import choose_resources
 
 
 def main():
@@ -58,15 +58,8 @@ def main():
 
 
 def page_home():
-    st.title("🏠 Home")
-
-
-def page_journal():
-    st.title("📝 Journal")
-
     with st.container():
-        st.title("Home
-                 ")
+        st.title("Home")
         '''
         ##### Welcome to Better.Me. Please login below to access your personal AI powered diary.
         '''
@@ -106,7 +99,7 @@ def page_journal():
 
 
 def page_previous_journals():
-    st.title("📕 Previous Journals")
+    st.title("Previous journals")
 
     mood_box = {
         "anger": st.error,
@@ -135,7 +128,7 @@ def page_previous_journals():
 
 
 def page_analytics():
-    st.title("📊 Analytics")
+    st.title("Analytics")
     
     col0, col1 = st.columns(2)
     
@@ -212,13 +205,16 @@ def page_analytics():
     
 
 def page_resources():
-    st.title("📚 Recommended Resources")
+
+    st.title("Resources")
     col1, col2, col3 = st.columns(3)
 
     # TODO: Change based on analytics page
     mood = "anger"
 
-    p1, p2, p3 = choose_resources(mood), choose_resources(mood), choose_resources(mood)
+    p1 = choose_resources(mood)
+    p2 = choose_resources(mood)
+    p3 = choose_resources(mood)
 
     with col1:
         st.header(p1.title)
@@ -234,27 +230,6 @@ def page_resources():
         st.header(p3.title)
         st.write(p3.description)
         st.markdown("<a href=\"p3.url\"> Learn More </a>", unsafe_allow_html=True)
-
-    st.markdown("---")
-    st.title("Recommended Support")
-
-    col4, col5, col6 = st.columns(3)
-    s1, s2, s3 = choose_support(0), choose_support(1), choose_support(2)
-    with col4:
-        st.header(s1.title)
-        st.write(s1.description)
-        st.markdown("<a href=\"s1.url\"> Learn More </a>", unsafe_allow_html=True)
-
-    with col5:
-        st.header(s2.title)
-        st.write(s2.description)
-        st.markdown("<a href=\"s1.url\"> Learn More </a>", unsafe_allow_html=True)
-
-    with col6:
-        st.header(s3.title)
-        st.write(s3.description)
-        st.markdown("<a href=\"s3.url\"> Learn More </a>", unsafe_allow_html=True)
-
 
 if __name__ == "__main__":
     main()
